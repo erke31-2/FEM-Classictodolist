d76be1e5e7df97082cc39bac24bd4db46a2afb04
Kaung Set <121620228+erke31-2@users.noreply.github.com>
2023-06-18 23:05:14 +0630

    Delete Footer.tsx

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <p className="text-center text-DarkGrayishBlue dark:bg-VeryDarkBlueDark bg-VeryLightGray">
-      Drag and drop to reorder the list
-    </p>
-  );
-};
-export default Footer;
